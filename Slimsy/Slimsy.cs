b637f53f5fd59cc2f8d6d0fcbef2e177658e4691
Jeavon Leopold <jeavon@crumpled-dog.com>
2014-05-22 12:39:26 +0100

    Adding GetResponsiveImageUrl helper method

--- Slimsy/Slimsy.cs
+++ Slimsy/Slimsy.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Slimsy
+﻿namespace Slimsy
 {
-    public class Slimsy
+    using Umbraco.Core.Models;
+    using Umbraco.Web;
+    using Umbraco.Web.Models;
+
+    public static class Slimsy
     {
+        public static string GetResponsiveImageUrl(this IPublishedContent publishedContent, int width, int height)
+        {
+            return publishedContent.GetCropUrl(
+                width,
+                height,
+                ratioMode: ImageCropRatioMode.Height,
+                furtherOptions: "&slimmage=true");
+        }
+
+        public static string GetResponsiveImageUrl(this IPublishedContent publishedContent, int width, int height, string propertyAlias)
+        {
+            return publishedContent.GetCropUrl(
+                width,
+                height,
+                propertyAlias,
+                ratioMode: ImageCropRatioMode.Height,
+                furtherOptions: "&slimmage=true");
+        }
+
     }
 }
