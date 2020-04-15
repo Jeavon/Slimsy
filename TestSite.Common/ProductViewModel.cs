2d506479db58838d8e92180921dc24a8f17744e7
Jeavon Leopold <jeavon@crumpled-dog.com>
2020-04-15 13:50:14 +0100

    Added example of using SlimsyComponent with a RenderController

---
+++ TestSite.Common/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Web.Models;
+
+namespace TestSite.Common
+{
+    public class ProductViewModel : ContentModel
+    {
+        public ProductViewModel(IPublishedContent content) : base(content) { }
+        public IHtmlString PhotoSrc { get; set; }
+        public IHtmlString PhotoSrcSetUrls { get; set; }
+    }
+}
